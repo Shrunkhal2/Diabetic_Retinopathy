caf8c6cf23d33b815e57304f4e0872392a259ec3
Shrunkhal Lokhande <shrunkhallokhande@Shrunkhals-MacBook-Air.local>
2026-04-10 22:34:34 +0530

    10/4

--- DR_WEBSITE/backend/middleware/adminMiddleware.js
+++
@@ -1,9 +0,0 @@
-const adminOnly = (req, res, next) => {
-  if (req.doctor && req.doctor.role === "admin") {
-    next();
-  } else {
-    res.status(403).json({ message: "Admin access only" });
-  }
-};
-
-module.exports = adminOnly;
