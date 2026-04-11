b68bd1ce19b690e414c7210045b78aeba9c7ca7b
Prajwal-pardeshi <prajwalpardeshi26@gmail.com>
2026-04-11 14:39:10 +0530

    directory change

--- src/pages/Unauthorized.jsx
+++
@@ -1,9 +0,0 @@
-const Unauthorized = () => {
-  return (
-    <div className="h-screen flex items-center justify-center text-red-600 text-xl">
-      Access Denied 🚫
-    </div>
-  );
-};
-
-export default Unauthorized;
